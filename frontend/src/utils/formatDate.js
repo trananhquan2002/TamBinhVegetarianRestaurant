b7c3a8cd7f73268ae946b3929156a7808569ba4c
Tran Anh Quan <trananhquan23702@gmail.com>
2026-01-20 01:19:25 +0700

    Fix error timezone

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,17 +1,22 @@
 export const formatDateTimeVN = (dateString) => {
   if (!dateString) return { time: '--:--', date: '--/--/----' }
-  const dateObj = new Date(dateString)
-  const time = dateObj.toLocaleTimeString('en-GB', {
+
+  // ÉP timezone VN
+  const dateObj = new Date(dateString + '+07:00')
+
+  const time = dateObj.toLocaleTimeString('vi-VN', {
     hour: '2-digit',
     minute: '2-digit',
     hour12: false,
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  const date = dateObj.toLocaleDateString('en-GB', {
+
+  const date = dateObj.toLocaleDateString('vi-VN', {
     day: '2-digit',
     month: '2-digit',
     year: 'numeric',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
+
   return { time, date }
 }
